8ad3a1ebd58415c9f93dfbaa3c4635da7e6eedba
Md Owais Farhan Akhter <mdowaisfarhanakhter.cse2278@bppimt.ac.in>
2025-09-07 14:03:20 +0530

    dhamki commit

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const OPENWEATHER_API_KEY = "c20c452decd2e86e8ceeebd15dcb204a";
+
+// stealing is wrong and i'm still learning so don't steal my key or i'll find the key to your house and do things you wont ever imagine 
